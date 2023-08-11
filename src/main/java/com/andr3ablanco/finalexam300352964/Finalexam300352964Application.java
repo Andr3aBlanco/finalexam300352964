f85a042361c4da6d9b6dfecfde9d77db66e6abf5
Andrea Blanco <andr3ablanco@gmail.com>
2023-08-11 16:10:23 -0700

    Initial commit

---
+++ src/main/java/com/andr3ablanco/finalexam300352964/Finalexam300352964Application.java
@@ -0,0 +1,13 @@
+package com.andr3ablanco.finalexam300352964;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Finalexam300352964Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Finalexam300352964Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/andr3ablanco/finalexam300352964/Finalexam300352964ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andr3ablanco.finalexam300352964;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Finalexam300352964ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
