8b8bea86f5d4b4c00a36d3eb6673e806dfcae940
Andrea Blanco <andr3ablanco@gmail.com>
2023-08-11 18:37:39 -0700

    edit didi not work created test

--- src/test/java/com/andr3ablanco/finalexam300352964/Controllers/CoreControllerTest.java
+++ src/test/java/com/andr3ablanco/finalexam300352964/Controllers/CoreControllerTest.java
@@ -1,10 +1,27 @@
 package com.andr3ablanco.finalexam300352964.Controllers;
 
+import com.andr3ablanco.finalexam300352964.Repository.SaleRepository;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.ui.ExtendedModelMap;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.MapBindingResult;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CoreControllerTest {
+
+    @Mock
+    private SaleRepository saleRepository;
+
+    @InjectMocks
+    private CoreController coreController;
 
     @Test
     void showHome() {
@@ -12,6 +29,20 @@ class CoreControllerTest {
 
     @Test
     void showHome2() {
+
+//        Model model = new ExtendedModelMap();
+//        BindingResult bindingResult = new MapBindingResult(new HashMap<>(), "");
+//
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+//        Date saleDate = dateFormat.parse("2023-08-10");
+//
+//        when(bindingResult.hasErrors()).thenReturn(false);
+//
+//        coreController.saveEdit(model, bindingResult, 123, "2023-08-10", 10.5, "ITEM123", 1L, null);
+//
+//        Sale expectedSale = new Sale(123, "ITEM123", 10.5, saleDate, null);
+//        verify(saleRepository).updateSAle(1L, 10.5, "ITEM123");
+
     }
 
     @Test
